9c8cbbfa968ee0c600639a3477da96a891c13def
Maty333 <126540155+Maty333@users.noreply.github.com>
2023-04-19 15:04:53 -0300

    Add files via upload

---
+++ tp13 - entrega/src/conexion/db/ConexionSQL.java
@@ -0,0 +1,26 @@
+package conexion.db;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//--------------------------
+public class ConexionSQL {
+	public static void main(String[] args){
+	try{
+	    Class.forName("com.mysql.jdbc.Driver");
+	    DriverManager.getConnection("jdbc:mysql://localhost:3306/escuela","root","XXXX");
+	    System.out.println("Conexion exitosa!!");
+	    
+	}catch (ClassNotFoundException e){
+	    System.out.println("Error al cargar el controlador!!!!");
+	    e.printStackTrace();
+	    
+	}catch (SQLException e){
+	    System.out.println("Error en la conexion!!!!");
+	    e.printStackTrace();
+	}
+}
+}
+	
